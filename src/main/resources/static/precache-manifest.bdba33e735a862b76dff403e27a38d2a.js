6babfc10c14516cefdcd7f3cd91df30f8926c1f1
Grisyt <hxl164@outlook.com>
2022-07-01 18:21:29 -0600

    build change

---
+++ src/main/resources/static/precache-manifest.bdba33e735a862b76dff403e27a38d2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "faae60736acfeb4644d4",
+    "url": "/static/css/main.c14f91d4.chunk.css"
+  },
+  {
+    "revision": "faae60736acfeb4644d4",
+    "url": "/static/js/main.faae6073.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "588029891aebe935706a",
+    "url": "/static/css/2.18180b6c.chunk.css"
+  },
+  {
+    "revision": "588029891aebe935706a",
+    "url": "/static/js/2.58802989.chunk.js"
+  },
+  {
+    "revision": "ae53f87981aff75462ae",
+    "url": "/static/js/runtime~main.ae53f879.js"
+  },
+  {
+    "revision": "1f389cd14bc9a23c1ae489a3f4ca5ced",
+    "url": "/index.html"
+  }
+];
